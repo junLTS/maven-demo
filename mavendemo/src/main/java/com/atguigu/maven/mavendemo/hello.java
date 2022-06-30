b19be054eef7a51dbbc0712bd4ce096e2ed11538
jianjun <819447797@qq.com>
2022-06-30 10:23:26 +0800

    master commit

--- mavendemo/src/main/java/com/atguigu/maven/mavendemo/hello.java
+++ mavendemo/src/main/java/com/atguigu/maven/mavendemo/hello.java
@@ -2,7 +2,10 @@ package com.atguigu.maven.mavendemo;
 
 public class hello {
 
-
+    public static void main(String[] args) {
+        System.out.println("hello github!");
+        System.out.println("master test!");
+    }
     public String hello(String name ){
         return "hello " + name + "!" ;
     }
