2bf5e570a87c84ddc41a9ff34bc7be1e8891e2e2
jianjun <819447797@qq.com>
2022-06-30 10:22:27 +0800

    hot-fix commit

--- mavendemo/src/main/java/com/atguigu/maven/mavendemo/hello.java
+++ mavendemo/src/main/java/com/atguigu/maven/mavendemo/hello.java
@@ -2,6 +2,10 @@ package com.atguigu.maven.mavendemo;
 
 public class hello {
 
+    public static void main(String[] args) {
+        System.out.println("hello github!!");
+        System.out.println("hot-fix test");
+    }
 
     public String hello(String name ){
         return "hello " + name + "!" ;
